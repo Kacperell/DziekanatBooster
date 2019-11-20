1801234625192dfaecb2766791f8c5fb0e13fbe4
Olgierd <o.liberski@intive.com>
2019-11-20 22:21:36 +0100

    feat: heroku deploy setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
+const PORT = process.env.PORT || 4321
 
-app.set('port', 4321);
+app.set('port', PORT);
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening on localhost:${ server.address().port }`);
 });
