dfd26e19b262a78204db6025ac7a3392b447b3b8
Nihler <vanilor97@gmail.com>
2019-11-21 01:21:10 +0100

    Orders setup

---
+++ controllers/OrdersController.js
@@ -0,0 +1,14 @@
+const { Auth } = require("../firebase/index");
+const firebase = require("firebase");
+
+exports.newOrder = async (req, res) => {
+  const ref = firebase
+    .database()
+    .ref()
+    .child("orders");
+
+  console.log("========================================");
+  console.log(ref);
+  let orders = { id: 1, ornerNumber: 1, isActive: false };
+  let pushOrder = ref.push(orders);
+};
---
+++ routes/orders.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const OrdersController = require("../controllers/OrdersController");
+const { catchErrors } = require("../handlers/errorHandlers");
+
+router.get("/newOrder", OrdersController.newOrder);
+
+module.exports = router;
