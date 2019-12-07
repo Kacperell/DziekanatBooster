d85945c3a29f9cb771bb1e82a4ef2a6e9f5158e0
Kacper Lewandowski <kacperloczek3@gmail.com>
2019-12-07 15:10:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 4321;
 
 app.set('port', PORT);
-const server = app.listen(4321, () => {
+
+const server = app.listen(app.get('port'), () => {
     console.log(`Listening on localhost:${ server.address().port }`);
 });
-
-// const server = app.listen(3000, '192.168.1.7' || 'localhost', function () {
-//     console.log('Application worker ' + process.pid + ' started...');
-// });
