2b59b612533ab002f1c08815697f517726a74e6a
Dariusz K <heniulu10@gmail.com>
2019-12-30 23:17:36 +0100

    register validation added

--- server/controllers/utils/register.js
+++ server/controllers/utils/register.js
@@ -4,17 +4,42 @@ const {
 
 module.exports = async function register(req, res) {
     const auth = new Auth(req.body);
+    console.log(req.body);
     const {
         message,
         error
     } = await auth.register()
     console.log(error);
-    if (!error) {
+
+    if ((!error) && (req.body.password == req.body.password2)) {
         res.redirect('/student');
+    } else if ((req.body.password).length > 6) {
+        if (req.body.password !== req.body.password2) {
+            if (error) {
+                res.render('form.pug', {
+                    message: ['Passwords do not match', 'Email address is taken']
+                })
+            } else {
+                res.render('form.pug', {
+                    message: ['Passwords do not match']
+                })
+            }
+        }
     } else {
-        res.render('login.pug', {
-            message: error,
-            previousState: '/register'
-        })
+        if ((req.body.password).length < 6) {
+            if (req.body.password !== req.body.password2) {
+                res.render('form.pug', {
+                    message: ['Password must contain 6 characters', 'Email address already exists', 'Passwords do not match']
+                })
+            } else {
+                res.render('form.pug', {
+                    message: ['Password must contain 6 characters', 'Email address already exists']
+                })
+            }
+        } else {
+            res.render('form.pug', {
+                message: [error]
+            })
+        }
     }
 }
