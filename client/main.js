5804b62cb66653390fff4c867bc70b813653f046
Dariusz Karp <32850451+drekyyy@users.noreply.github.com>
2019-12-30 23:23:05 +0100

    register validation added (#21)

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
 import visit from './components/visit';
+import registerValidation from './components/registerValidation';
+import okayBackToRegister from './components/errorWindowPopUp';
 
 visit();
+registerValidation();
+okayBackToRegister();
